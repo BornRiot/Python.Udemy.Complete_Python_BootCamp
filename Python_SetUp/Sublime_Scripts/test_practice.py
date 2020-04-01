2e9908a5da6e5a416ef6b64538709e833699d5b8
BornRiot <mdacosata.devs@gmail.com>
2020-04-01 19:44:40 -0400

    C
    g

---
+++ Python_SetUp/Sublime_Scripts/test_practice.py
@@ -0,0 +1,12 @@
+import math
+
+# Numbers in python are used to perform mathematical calculations and includes the data types integers and floating
+# point decimals
+# Strings are a sequence of characters that can be indexed which begin at the index of zero. strings are immutable which
+# means that
+
+
+the_final = (5*2)**2+(.50-(1/4))
+
+print(the_final)
+print("This is a ver simple sentence to be printed on the screen")
