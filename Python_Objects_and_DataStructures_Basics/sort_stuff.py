46878cd436eae12c46128b3669eff49b2ff2144b
BornRiot <marvindacosta#hotmail.com>
2020-05-10 17:47:38 -0400

    Programmer that demonstrates using the Python Debugger

--- Python_Objects_and_DataStructures_Basics/sort_stuff.py
+++ Python_Objects_and_DataStructures_Basics/sort_stuff.py
@@ -1,3 +1,9 @@
 items = [1, 80, 4, 9, 6]
 items.sort()
 print(items)
+print("This is a line of text ")
+print("I just solve a [roblem that has been bothering me for years")
+print('P')
+print ("this is to test how loud tje keybpard will be when typing")
+print("This is a s ")
+print(3*25-19/2)
