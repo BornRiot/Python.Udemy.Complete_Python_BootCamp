cf1d19dccc9ba3e837112f5311e6bc73674b4717
BornRiot <marvindacosta#hotmail.com>
2020-06-01 13:48:58 -0400

    removed a line

--- methods_and_functions/function_excercises/function_10.py
+++ methods_and_functions/function_excercises/function_10.py
@@ -1,1 +1,9 @@
-print("This is something")
+"""My module docstring """
+some_string = "The sTirng"
+
+my_list = [x for x in some_string]
+for x in my_list:
+    if x.__eq__(" "):
+        my_list = my_list.pop(x.index(" "))
+print(my_list)
+print("This is an update to test Sublime Merge Pull Request")
