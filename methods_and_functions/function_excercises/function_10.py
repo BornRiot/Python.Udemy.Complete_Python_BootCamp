71d05db50f0f154f10b2c8514ff37dbc81a3a7cb
BornRiot <marvindacosta#hotmail.com>
2020-06-03 19:50:38 -0400

    Completed exercise 10 of section

--- methods_and_functions/function_excercises/function_10.py
+++ methods_and_functions/function_excercises/function_10.py
@@ -1,12 +1,24 @@
 """My module docstring """
-some_string = "The sTirng"
 
-my_list = [x for x in some_string]
-for x in my_list:
-    if x.__eq__(" "):
-        my_list = my_list.pop(x.index(" "))
-print(my_list)
-print("This is an update to test Sublime Merge Pull Request")
-print("Register new branch")
-print("this is a change to the file")
-print("This is just some more changes  ")
+"""My module docstring """
+
+
+def myfunc(*args):
+    mynewstring = ''
+
+    mystring = str(args[0])
+
+    for num in range(len(args[0])):
+
+        if num % 2 == 0:
+
+            mynewstring = mynewstring + mystring[num].upper()
+
+        else:
+
+            mynewstring = mynewstring + mystring[num].lower()
+
+    return mynewstring
+
+
+print(myfunc("ringside"))
