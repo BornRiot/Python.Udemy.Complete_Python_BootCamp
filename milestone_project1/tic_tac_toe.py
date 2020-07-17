4356bdc8d0039f99dab89c695ab4d34106e9deae
BornRiot <marvindacosta#hotmail.com>
2020-07-17 02:54:47 -0400

    changed name of my_attempt file

--- milestone_project1/tic_tac_toe.py
+++ milestone_project1/tic_tac_toe.py
@@ -1,8 +1,10 @@
 """
-Module docstring
+This module is used to create my first project in the course.
+The projects calls for creating a tic-tac-toe game using python
 """
-# define the board
+# define place elements in the  board
 the_table = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 
 # Display the game board
 def print_game_table(game_table):
@@ -13,4 +15,15 @@ def print_game_table(game_table):
     print(game_table[6] + "|" + game_table[7] + '|' + game_table[8])
 
 
+print_game_table(the_table)
+
+
+def replacement_choice(game_list, position):
+    """Func string"""
+    user_placement = input("Type a string to place at position:")
+    game_list[position] = user_placement
+    return game_list
+
+
+change_stuff = replacement_choice(the_table, 8)
 print_game_table(the_table)
