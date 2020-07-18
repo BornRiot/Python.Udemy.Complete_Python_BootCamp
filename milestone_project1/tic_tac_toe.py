0930f7d9c32d8780f9286a05977c09b982ed7131
BornRiot <marvindacosta#hotmail.com>
2020-07-18 18:29:47 -0400

    made cganed to file tic_tac_toe.py

--- milestone_project1/tic_tac_toe.py
+++ milestone_project1/tic_tac_toe.py
@@ -3,7 +3,7 @@ This module is used to create my first project in the course.
 The projects calls for creating a tic-tac-toe game using python
 """
 # define place elements in the  board
-the_table = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+the_table = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
 
 # Display the game board
@@ -18,12 +18,40 @@ def print_game_table(game_table):
 print_game_table(the_table)
 
 
-def replacement_choice(game_list, position):
+def replacement_choice(game_table, position):
     """Func string"""
     user_placement = input("Type a string to place at position:")
-    game_list[position] = user_placement
-    return game_list
+    game_table[position] = user_placement
+    return game_table
 
 
-change_stuff = replacement_choice(the_table, 8)
+def game_on_choice():
+    """Function docstring"""
+    choice = "Wrong"
+
+    while choice not in ['Y', 'N']:
+        choice = input("Keep playing: (Y or N) ")
+        if choice not in ['Y', 'N']:
+            print("Sorry, I don't understand. Please choose Y or N")
+    if choice == 'Y':
+        return True
+    else:
+        print("Game Over")
+        return False
+
+
+game_on = True
+while game_on:
+    take_position = int(input("Please enter a position to be replace(0-8): "))
+
+    the_table = replacement_choice(the_table, take_position)
+
+    print_game_table(the_table)
+
+    game_on = game_on_choice()
+
 print_game_table(the_table)
+
+
+def new_func(some_arg):
+    print(some_arg)
