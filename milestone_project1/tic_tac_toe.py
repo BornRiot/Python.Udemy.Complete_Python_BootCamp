a6edb631b9c715aaff482c98ea3f139550ae68c3
BornRiot <marvindacosta#hotmail.com>
2020-07-19 16:33:37 -0400

    Working on two player version of code

--- milestone_project1/tic_tac_toe.py
+++ milestone_project1/tic_tac_toe.py
@@ -2,24 +2,24 @@
 This module is used to create my first project in the course.
 The projects calls for creating a tic-tac-toe game using python
 """
-# define place elements in the  board
-the_table = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+# define place elements in the  board. Visual list star at 1 not 0
+the_table = [None, "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-# Display the game board
+# Code to display the game board
 def print_game_table(game_table):
-    print(game_table[0] + "|" + game_table[1] + '|' + game_table[2])
+    print(game_table[1] + "|" + game_table[2] + '|' + game_table[3])
     print("-|-|-")
-    print(game_table[3] + "|" + game_table[4] + '|' + game_table[5])
+    print(game_table[4] + "|" + game_table[5] + '|' + game_table[6])
     print("-|-|-")
-    print(game_table[6] + "|" + game_table[7] + '|' + game_table[8])
+    print(game_table[7] + "|" + game_table[8] + '|' + game_table[9])
 
 
 print_game_table(the_table)
 
 
 def replacement_choice(game_table, position):
-    """Func string"""
+    """Code for replacing user choice when option is chosen """
     user_placement = input("Type a string to place at position:")
     game_table[position] = user_placement
     return game_table
@@ -30,9 +30,9 @@ def game_on_choice():
     choice = "Wrong"
 
     while choice not in ['Y', 'N']:
-        choice = input("Keep playing: (Y or N) ")
+        choice = input("Keep playing?: (Y or N): ")
         if choice not in ['Y', 'N']:
-            print("Sorry, I don't understand. Please choose Y or N")
+            print("Sorry, I don't understand. Please choose Y or N:")
     if choice == 'Y':
         return True
     else:
@@ -42,7 +42,7 @@ def game_on_choice():
 
 game_on = True
 while game_on:
-    take_position = int(input("Please enter a position to be replace(0-8): "))
+    take_position = int(input("Please enter a position to be replaced(1-9): "))
 
     the_table = replacement_choice(the_table, take_position)
 
@@ -51,7 +51,3 @@ while game_on:
     game_on = game_on_choice()
 
 print_game_table(the_table)
-
-
-def new_func(some_arg):
-    print(some_arg)
