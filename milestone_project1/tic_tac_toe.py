fa2aee70c9a9f8579f9e12062d223a1e9ef860d4
BornRiot <marvindacosta#hotmail.com>
2020-07-23 19:52:01 -0400

    My attempt to solve tic tac toe. Needs several improvements/additions

--- milestone_project1/tic_tac_toe.py
+++ milestone_project1/tic_tac_toe.py
@@ -6,7 +6,6 @@ The projects calls for creating a tic-tac-toe game using python
 the_table = [None, "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-# Code to display the game board
 def print_game_table(game_table):
     print(game_table[1] + "|" + game_table[2] + '|' + game_table[3])
     print("-|-|-")
@@ -15,13 +14,10 @@ def print_game_table(game_table):
     print(game_table[7] + "|" + game_table[8] + '|' + game_table[9])
 
 
-print_game_table(the_table)
-
-
-def replacement_choice(game_table, position):
+def replacement_choice(game_table, position, piece):
     """Code for replacing user choice when option is chosen """
-    user_placement = input("Type a string to place at position:")
-    game_table[position] = user_placement
+
+    game_table[position] = piece
     return game_table
 
 
@@ -30,7 +26,7 @@ def game_on_choice():
     choice = "Wrong"
 
     while choice not in ['Y', 'N']:
-        choice = input("Keep playing?: (Y or N): ")
+        choice = input("Keep playing? (Y or N): ")
         if choice not in ['Y', 'N']:
             print("Sorry, I don't understand. Please choose Y or N:")
     if choice == 'Y':
@@ -40,14 +36,34 @@ def game_on_choice():
         return False
 
 
-game_on = True
-while game_on:
+player_one_piece = input("Player 1, please select X or O to play with: ")
+
+
+def player_one():
     take_position = int(input("Please enter a position to be replaced(1-9): "))
 
-    the_table = replacement_choice(the_table, take_position)
+    replacement_choice(the_table, take_position, player_one_piece)
 
     print_game_table(the_table)
 
-    game_on = game_on_choice()
 
-print_game_table(the_table)
+def player_two():
+    player_two_piece = ""
+    if player_one_piece == 'X':
+        player_two_piece = 'O'
+    elif player_one_piece == 'O':
+        player_two_piece = 'X'
+
+    take_position = int(input("Player 2, please enter a position to be replaced: "))
+
+    replacement_choice(the_table, take_position, player_two_piece)
+
+    print_game_table(the_table)
+
+
+game_on = True
+while game_on:
+    player_one()
+    player_two()
+
+    game_on = game_on_choice()
